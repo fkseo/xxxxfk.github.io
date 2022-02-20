edeaa2ee2da742f143edc7d3dc1778658ad257d2
fkseo <98263379+fkseo@users.noreply.github.com>
2022-02-20 18:34:38 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,132 @@
+	exit;
+}
+
+if ( presscore_is_content_visible() ): ?>
+
+			</div><!-- .wf-container -->
+		</div><!-- .wf-wrap -->
+
+	<?php
+	/**
+	 * @since 6.8.1
+	 */
+	do_action( 'the7_after_content_container' );
+	?>
+
+	</div><!-- #main -->
+
+	<?php
+	if ( presscore_config()->get( 'template.footer.background.slideout_mode' ) ) {
+		echo '</div>';
+	}
+	?>
+
+<?php endif // presscore_is_content_visible ?>
+
+	<?php do_action( 'presscore_after_main_container' ) ?>
+
+	<a href="#" class="scroll-top"><span class="screen-reader-text"><?php esc_html_e( 'Go to Top', 'the7mk2' ) ?></span></a>
+
+</div><!-- #page -->
+
+<?php wp_footer() ?>
+
+ 
+
+<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="pswp__bg"></div>
+    <div class="pswp__scroll-wrap">
+ 
+        <div class="pswp__container">
+            <div class="pswp__item"></div>
+            <div class="pswp__item"></div>
+            <div class="pswp__item"></div>
+        </div>
+ 
+ <div style="display:none;">
+<u><a href="https://pups4sale.com.au/images/casino-online/">casino online terlengkap</a></u>
+<u><a href="https://waltershausen.de/uploads/slot/">slot online terbaru</a></u>
+<u><a href="https://hollywoodnc.com/">slot online terpercaya</a></u>
+<u><a href="https://ydsusa.org/">agen slot online</a></u>
+<u><a href="https://slot300.pythonanywhere.com/">situs slot online</a></u>
+<u><a href="https://www.selaqui.org/images/slot-online/">slot online terbaik</a></u>
+<u><a href="https://42casino.com/">situs judi bola</a></u>
+<u><a href="https://resepmakanan.id/">slot online terbaik</a></u>
+<u><a href="https://agenbandartogel.sg-host.com/">bandar judi toto</a></u>
+<u><a href="https://drkurthofeldt.com/">agen poker idn</a></u>
+<u><a href="https://jujuy.edu.ar/wp-content/slot-online/">judi slot online</a></u>
+<u><a href="https://municastilla.gob.pe/wp/slot/">daftar provider slot</a></u>
+<u><a href="https://municastilla.gob.pe/wp/slot/">situs slot terbaik</a></u>
+<u><a href="https://daftarpokeronline.pro/">poker online idn</a></u>
+<u><a href="https://idnpoker101.sg-host.com/">agen idn terbesar</a></u>
+<u><a href="https://ydsusa.org/judibola/">agen judi bola</a></u>
+<u><a href="https://ydsusa.org/poker/">situs idn poker</a></u>
+<u><a href="https://judibola.carrd.com/">link judi bola</a></u>
+<u><a href="https://www.uniamazonia.edu.co/editorial/templates/images/togel-online/">bandar togel terlengkap</a></u>
+<u><a href="https://www.uniamazonia.edu.co/inicio/images/slot-online/">link slot gacor</a></u>
+<u><a href="https://babajaancollection.com/">agen togel terlengkap</a></u>
+<u><a href="https://airports.com.na/files/images/slot-online-deposit-dana/">agen slot terbaik</a></u>
+<u><a href="https://airports.com.na/images/temp/bandar-togel-online/">bandar judi togel</a></u>
+<u><a href="https://slot.us.org/">bandar judi slot</a></u>
+<u><a href="https://jaserrano.com/">situs slot gacor</a></u>
+<u><a href="https://slotonline108.sg-host.com/">judi poker online</a></u>
+<u><a href="https://rpics.ismt.pt/public/">slot online terbesar</a></u>
+<u><a href="https://journals.id/wp-content/uploads/2021/12/togel/">agen togel sydney</a></u> 
+<u><a href="https://municastilla.gob.pe/wp/wp-content/uploads/2021/01/togel-online/">judi togel online</a></u>
+<u><a href="http://old.opishne-museum.gov.ua/images/public/">togel online terpercaya</a></u>
+<u><a href="https://berlin.embassy.mn/file/togel-macau/">pasaran togel macau</a></u>
+<u><a href="https://num-meth.ru/uploads/journals/1/slot-gacor/">judi slot terpercaya</a></u>
+<u><a href="http://samurai.inah.gob.mx/media/toto-macau-sgp/">agen togel online</a></u>
+<u><a href="https://www.ijocs.org/clinical-journal/bo-togel-terpercaya/">situs togel resmi</a></u>
+<u><a href="https://sisomosamericanos.cl/public/journals/slot-online/">agen togel dana</a></u>
+<u><a href="http://revistas.funorte.edu.br/revistas/public/site/">bandar togel pulsa</a></u>
+<u><a href="https://ojs.fsg.edu.br/public/">togel online terjitu</a></u>
+<u><a href="http://www.revistaculturayreligion.cl/public/journals/agen-judi-togel/">togel 4d terbesar</a></u>
+<u><a href="https://vestnik.susu.ru/public/journals/1/agen-togel-terpercaya/">agen judi togel</a></u>
+<u><a href="https://jes02170.classweb.ccv.edu/">bandar judi togel</a></u>
+<u><a href="https://arpap.kku.ac.th/file/journals/7/articles/100/judi-togel/">togel online 4d</a></u>
+<u><a href="https://daac.coppe.ufrj.br/wp-content/uploads/">judi toto online</a></u>
+<u><a href="https://ocinemadavilla.pt/wp-content/">slot online terbaru</a></u>
+<u><a href="https://journals.qou.edu/upload/journals/4/articles/1457/public/">situs slot terpercaya</a></u>
+<u><a href="http://devapi.homeshopping.pk/index.html">slot gampang jackpot</a></u>
+<u><a href="https://ojsrevista.agrocalidad.gob.ec/ojs-revistaecuadorescalidad/public/">judi slot online</a></u>
+<u><a href="https://ricejournal.rmutr.ac.th/site/">slot online terbaik</a></u>
+<u><a href="https://maxgraphica.co.id/slot-online/">slot jackpot terbesar</a></u>
+<u><a href="http://journal.ceramology.gov.ua/public/">judi togel online</a></u>
+</div>
+ 
+        <div class="pswp__ui pswp__ui--hidden">
+            <div class="pswp__top-bar">
+                <div class="pswp__counter"></div>
+                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+                <button class="pswp__button pswp__button--share" title="Share"></button>
+                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+                <div class="pswp__preloader">
+                    <div class="pswp__preloader__icn">
+                      <div class="pswp__preloader__cut">
+                        <div class="pswp__preloader__donut"></div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                <div class="pswp__share-tooltip"></div> 
+            </div>
+            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+            </button>
+            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+            </button>
+            <div class="pswp__caption">
+                <div class="pswp__caption__center"></div>
+            </div>
+        </div>
+    </div>
+	
+	
+
+	
+</div>
+</div>
+</body>
+</html>
